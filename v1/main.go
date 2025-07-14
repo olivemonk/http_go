2f466a54af6dc2a99eb1d5a0b597c55636c7711e
credmonk <ganesh.prajapati@credgenics.com>
2025-07-14 18:31:31 +0530

    feat: http request parsing and simple hello world response

--- v1/main.go
+++ v1/main.go
@@ -1,22 +1,51 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net"
+	"bufio"
+	"fmt"
+	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close() // ensure the connection is closed when done
-	data, err := io.ReadAll(conn)
+
+	reader := bufio.NewReader(conn)
+
+	// read req line
+	reqLine, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("Read error: %v", err)
 		return
 	}
-	log.Printf("Received data: %s", data)
-	_, err = conn.Write(data) // echo back
+	reqLine = strings.TrimSpace(reqLine)
+	parts := strings.Split(reqLine, " ")
+	if len(parts) != 3 || parts[0] != "GET" || parts[1] != "/" || parts[2] != "HTTP/1.0" {
+		conn.Write([]byte("HTTP/1.0 400 Bad Request\r\n\r\nInvalid request"))
+		return
+	}
+	
+	// read headers 
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("Header read error: %v", err)
+			return
+		}
+		line = strings.TrimSpace(line)
+		if line == "" { // end of headers
+			break
+		}
+	}
+
+	// construct and send the response
+	body := "Hello, World!\n"
+	response := fmt.Sprintf("HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
+	_, err = conn.Write([]byte(response))
 	if err != nil {
 		log.Printf("Write error: %v", err)
+		return
 	}
 }
 
@@ -27,6 +56,8 @@ func main() {
 	}
 
 	defer listener.Close() // ensure the listener is closed when done
+	log.Println("HTTP/1.0 server listening on :8080")
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
